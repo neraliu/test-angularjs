ad19069928c506ce6d28e74ef619b1386c689667
Nera Liu <neraliu@gmail.com>
2015-09-16 19:16:43 +0800

    add notes to files

--- angular-phonecat-001/app/js/controllers.js
+++ angular-phonecat-001/app/js/controllers.js
@@ -7,13 +7,13 @@ phonecatApp.controller('PhoneListCtrl', function ($scope) {
   $scope.phones = [
     {'name': 'Nexus S',
      'snippet': 'Fast just got faster with Nexus S.',
-     'data1':'\'"><&',
-     'data2':'\'"><&',
+     'data1':'\'" ><&',
+     'data2':'\'" ><&',
      'data3':'" style="color:red',
      'data4':'\' style="color:red',
      'data5':' style="color:red"',
-     'data6':'behavior: url(iepngfix.htc);',
-     'data7':':;()',
+     'data6':'x:expression(alert(0));',
+     'data7':':;[]{}()',
      'data8':'javascript:alert(0)',
      'data9':'\''
     }
